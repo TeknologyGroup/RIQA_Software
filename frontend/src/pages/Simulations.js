4bdd2c0d7f3041d69a164a630df2f232ceb31cc0
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-23 10:44:55 +0100

    Create Simulations.js

---
+++ frontend/src/pages/Simulations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Simulations = () => {
+  return (
+    <div>
+      <h1>Simulations Page</h1>
+      <p>Run and manage simulations here.</p>
+    </div>
+  );
+};
+
+export default Simulations;
