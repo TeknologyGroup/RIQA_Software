fc283871e6279448e61bc112df1f2b12d5eb3664
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-23 14:59:33 +0100

    Create Documentation.js

---
+++ frontend/src/pages/Documentation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Documentation() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Documentazione</h1>
+      <p>Consulta la documentazione completa per utilizzare il software RIQA_Software.</p>
+    </div>
+  );
+}
+
+export default Documentation;
