75816a32a20160343a898c883f06ef12d026249b
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-23 10:45:25 +0100

    Create Visualizations.js

---
+++ frontend/src/pages/Visualizations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Visualizations = () => {
+  return (
+    <div>
+      <h1>Visualizations Page</h1>
+      <p>Explore visualizations here.</p>
+    </div>
+  );
+};
+
+export default Visualizations;
