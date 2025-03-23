264ab6519cbdac69e93284719fa1367ad5c8908a
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-23 14:49:16 +0100

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '20px', backgroundColor: '#333', color: '#fff', textAlign: 'center' }}>
+      <p>© 2025 RIQA_Software. Tutti i diritti riservati.</p>
+      <p>Licenza MIT - <a href="/docs" style={{ color: '#fff' }}>Vedi dettagli</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
