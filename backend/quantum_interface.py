00e38f551accf71f38ddc5f7a5c3f70db86c87a2
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-24 22:40:06 +0100

    Update quantum_interface.py

--- backend/quantum_interface.py
+++ backend/quantum_interface.py
@@ -13,3 +13,7 @@ qc.measure_all()
 result = execute(qc, Aer.get_backend('qasm_simulator')).result()
 
 return result.get_counts()
+
+from qiskit import IBMQ
+IBMQ.enable_account("TUO_IBMQ_TOKEN")
+backend = IBMQ.get_backend("ibmq_lima")  # Esempio
