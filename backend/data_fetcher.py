291963ee3e9ae6103d8b2390d05e0d2464875623
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-24 23:36:46 +0100

    Update data_fetcher.py

--- backend/data_fetcher.py
+++ backend/data_fetcher.py
@@ -1,6 +1,8 @@
 import requests
 from flask import jsonify
+from dotenv import load_dotenv
 
+load_dotenv()
 def search_arxiv(query):
     url = f"http://export.arxiv.org/api/query?search_query={query}"
     response = requests.get(url)
