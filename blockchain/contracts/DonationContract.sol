1143e2a4ac879e1277363bea73f9d65b5bc178f0
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-24 22:26:02 +0100

    Update DonationContract.sol

--- blockchain/contracts/DonationContract.sol
+++ blockchain/contracts/DonationContract.sol
@@ -1,15 +1,7 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-contract DonationContract {
-    address public owner;
-    uint256 public totalDonations;
+function donate() external payable {
+    totalDonations += msg.value;
+    emit Donated(msg.sender, msg.value);
 
-    constructor() {
-        owner = msg.sender;
-    }
 
-    function donate() external payable {
-        totalDonations += msg.value;
-    }
 }
