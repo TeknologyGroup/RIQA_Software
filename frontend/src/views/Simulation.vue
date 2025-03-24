1003f0ef1cff5b358fddab7f4275b406ebc02ece
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-24 22:13:18 +0100

    Create Simulation.vue

---
+++ frontend/src/views/Simulation.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Simulazione Quantistica</h2>
+    <button @click="runSimulation">Avvia</button>
+    <div v-for="(msg, index) in messages" :key="index">{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { messages: [] };
+  },
+  methods: {
+    runSimulation() {
+      const socket = new WebSocket("ws://localhost:5000/ws/simulation");
+      socket.onmessage = (event) => this.messages.push(event.data);
+    }
+  }
+};
+</script>
