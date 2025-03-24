7b1371b2eb2fd46301ec13813fd62adc9f848d2c
Martyn Battista <109523724+TeknologyGroup@users.noreply.github.com>
2025-03-24 22:22:46 +0100

    Create SimulationChart.vue

---
+++ frontend/src/components/SimulationChart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Plotly :data="plotData" :layout="layout" />
+  </div>
+</template>
+
+<script>
+import { Plotly } from 'vue-plotly'
+
+export default {
+  data() {
+    return {
+      plotData: [{ x: [1, 2, 3], y: [2, 4, 6], type: 'scatter' }],
+      layout: { title: 'Risultati Simulazione' }
+    }
+  }
+}
+</script>
